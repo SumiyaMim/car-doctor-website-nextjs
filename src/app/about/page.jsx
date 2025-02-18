b34fcf9aca4b0cd27659fe84f146313d6b5d8ff5
SumiyaMim <sumiyaislam1093@gmail.com>
2025-02-18 20:14:15 +0600

    update about and services

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,42 +1,10 @@
 import React from 'react'
-import Image from 'next/image'
+import About from '../components/home/About'
 
-export default function About() {
+export default function AboutPage() {
   return (
-    <div className="max-w-7xl mx-auto px-6 md:px-8 lg:px-12 hero min-h-screen pt-20 pb-12 lg:pb-32">
-      <div className="flex flex-col lg:flex-row gap-10">
-        <div className="lg:w-1/2 relative">
-          <Image
-            src="/assets/images/about_us/person.jpg"
-            alt="Person"
-            className="w-4/5 lg:w-3/4 h-full rounded-md"
-            layout="responsive"
-            width={500}
-            height={500}
-          />
-          <Image
-            src="/assets/images/about_us/parts.jpg"
-            alt="Parts"
-            className="w-1/2 absolute right-0 lg:right-1 top-2/3 lg:top-3/4 rounded-md border-8 border-white"
-            layout="intrinsic"
-            width={500}
-            height={500}
-          />
-        </div>
-        <div className="lg:w-1/2 space-y-5 pt-14 md:pt-28 lg:pt-0">
-          <h3 className="text-base md:text-lg text-[#FF3811] font-bold mb-2">About us</h3>
-          <h1 className="text-3xl md:text-4xl md:w-3/4 font-bold">We are qualified & of experience in this field</h1>
-          <p className="pt-6 pb-2 text-[#737373] text-sm leading-6 text-justify lg:w-10/12">
-            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
-          </p>
-          <p className="pb-6 text-[#737373] text-sm leading-6 text-justify lg:w-10/12">
-            The majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
-          </p>
-          <button className="bg-[#FF3811] px-5 py-3 rounded font-semibold text-white text-xs md:text-sm">
-            Get More Info
-          </button>
-        </div>
-      </div>
+    <div className="max-w-7xl mx-auto px-6 md:px-8 lg:px-12">
+      <About/>
     </div>
   )
 }
